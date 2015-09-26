12e065feded6c0a351b14f811e80645ac64059a6
E. van Harten <evharten@snelmail.nl>
2015-09-26 05:19:06 +0200

    Added initial files

---
+++ Sounds/ComixSounds.lua
@@ -0,0 +1,130 @@
+-- Comix Sounds 
+ComixSounds = {};
+ComixSounds[1] = "CritSounds\\Crit1.ogg"
+ComixSounds[2] = "CritSounds\\Crit2.ogg"
+ComixSounds[3] = "CritSounds\\Crit3.ogg"
+ComixSounds[4] = "CritSounds\\Crit4.ogg"
+ComixSounds[5] = "CritSounds\\Crit5.ogg"
+ComixSounds[6] = "CritSounds\\Crit6.ogg"
+ComixSounds[7] = "CritSounds\\Crit7.ogg"
+ComixSounds[7] = "CritSounds\\Crit8.ogg"
+
+-- Comix Special Sounds --
+ComixSpecialSounds = {}
+ComixSpecialSounds[1] = "SpecialSounds\\Battleshout.ogg"
+ComixSpecialSounds[2] = "SpecialSounds\\DemoShout.ogg"
+ComixSpecialSounds[3] = "SpecialSounds\\DemoShout2.ogg"
+ComixSpecialSounds[4] = "SpecialSounds\\impressive.ogg"
+ComixSpecialSounds[5] = "SpecialSounds\\dr_evil.ogg"
+ComixSpecialSounds[6] = "SpecialSounds\\game.ogg"
+ComixSpecialSounds[7] = "SpecialSounds\\allyourbase.ogg"
+ComixSpecialSounds[8] = "SpecialSounds\\boing.ogg"
+ComixSpecialSounds[9] = "SpecialSounds\\welldone.ogg"
+ComixSpecialSounds[10] = "SpecialSounds\\muffinman.ogg"
+ComixSpecialSounds[11] = "SpecialSounds\\speedy.ogg"
+ComixSpecialSounds[12] = "SpecialSounds\\finishim.ogg"
+ComixSpecialSounds[13] = "SpecialSounds\\bam.ogg"
+ComixSpecialSounds[14] = "SpecialSounds\\TYM.ogg"
+ComixSpecialSounds[15] = "SpecialSounds\\bucketonothing.ogg"
+ComixSpecialSounds[16] = "SpecialSounds\\lair.ogg"
+ComixSpecialSounds[17] = "SpecialSounds\\drum.ogg"
+ComixSpecialSounds[18] = "SpecialSounds\\ExtraLife.ogg"
+ComixSpecialSounds[19] = "SpecialSounds\\drama.ogg"
+
+
+-- Comix Res Sounds --
+ComixResSounds = {}
+ComixSpecialSounds[1] = "Ressounds\\res1.ogg"
+ComixSpecialSounds[1] = "Ressounds\\res2.ogg"
+
+
+-- Comix Ability Sounds --
+ComixAbilitySounds = {}
+ComixAbilitySounds[1] = "AbilitySounds\\cookie.ogg"
+ComixAbilitySounds[2] = "AbilitySounds\\frosty.ogg"
+ComixAbilitySounds[3] = "AbilitySounds\\flare.ogg"
+
+-- Comix Ready Check Sounds --
+ComixReadySounds = {}
+ComixReadySounds[1] = "ReadySounds\\ready1.ogg"
+ComixReadySounds[2] = "ReadySounds\\ready2.ogg"
+ComixReadySounds[3] = "ReadySounds\\ready3.ogg"
+ComixReadySounds[4] = "ReadySounds\\ready4.ogg"
+ComixReadySounds[5] = "ReadySounds\\ready5.ogg"
+ComixReadySounds[5] = "ReadySounds\\ready6.ogg"
+
+
+-- Comix Death Sounds --
+ComixDeathSounds = {}
+ComixDeathSounds[1] = "DeathSounds\\IOUS.ogg"
+ComixDeathSounds[2] = "DeathSounds\\mariodie.ogg"
+ComixDeathSounds[3] = "DeathSounds\\pacmandie.ogg"
+ComixDeathSounds[4] = "DeathSounds\\whoa.ogg"
+ComixDeathSounds[5] = "DeathSounds\\whoops.ogg"
+ComixDeathSounds[6] = "DeathSounds\\ohno.ogg"
+ComixDeathSounds[7] = "DeathSounds\\etpm.ogg"
+ComixDeathSounds[8] = "DeathSounds\\alexdeath.ogg"
+ComixDeathSounds[9] = "DeathSounds\\zelda.ogg"
+ComixDeathSounds[10] = "DeathSounds\\mariodie2.ogg"
+ComixDeathSounds[10] = "DeathSounds\\death1.ogg"
+ComixDeathSounds[10] = "DeathSounds\\death2.ogg"
+
+-- Comix KillCount Sounds --
+ComixKillCountSounds = {}
+ComixKillCountSounds[1] =  "ComixKillCountSounds\\killingspree.ogg"
+ComixKillCountSounds[2] =  "ComixKillCountSounds\\rampage.ogg"
+ComixKillCountSounds[3] =  "ComixKillCountSounds\\multikill.ogg"
+ComixKillCountSounds[4] =  "ComixKillCountSounds\\unstoppable.ogg"
+ComixKillCountSounds[5] =  "ComixKillCountSounds\\ultrakill.ogg"
+ComixKillCountSounds[6] =  "ComixKillCountSounds\\dominating.ogg"
+ComixKillCountSounds[7] =  "ComixKillCountSounds\\godlike.ogg"
+ComixKillCountSounds[8] =  "ComixKillCountSounds\\ludicrouskill.ogg"
+ComixKillCountSounds[9] =  "ComixKillCountSounds\\monsterkill.ogg"
+
+
+-- Comix Zone Sounds --
+ComixZoneSounds = {}
+ComixZoneSounds[1] = "ZoneSounds\\zone1.ogg"
+ComixZoneSounds[2] = "ZoneSounds\\zone2.ogg"
+ComixZoneSounds[3] = "ZoneSounds\\zone3.ogg"
+ComixZoneSounds[4] = "ZoneSounds\\zone4.ogg"
+ComixZoneSounds[5] = "ZoneSounds\\zone5.ogg"
+ComixZoneSounds[6] = "ZoneSounds\\zone6.ogg"
+ComixZoneSounds[7] = "ZoneSounds\\zone7.ogg"
+ComixZoneSounds[8] = "ZoneSounds\\zone8.ogg"
+ComixZoneSounds[9] = "ZoneSounds\\zone9.ogg"
+ComixZoneSounds[10] = "ZoneSounds\\zone10.ogg"
+ComixZoneSounds[11] = "ZoneSounds\\zone11.ogg"
+ComixZoneSounds[12] = "ZoneSounds\\zone12.ogg"
+ComixZoneSounds[13] = "ZoneSounds\\zone13.ogg"
+ComixZoneSounds[14] = "ZoneSounds\\zone14.ogg"
+ComixZoneSounds[15] = "ZoneSounds\\zone15.ogg"
+ComixZoneSounds[16] = "ZoneSounds\\zone16.ogg"
+ComixZoneSounds[17] = "ZoneSounds\\zone17.ogg"
+ComixZoneSounds[18] = "ZoneSounds\\zone18.ogg"
+ComixZoneSounds[18] = "ZoneSounds\\zone19.ogg"
+ComixZoneSounds[18] = "ZoneSounds\\zone20.ogg"
+
+-- Comix MCombat Sounds --
+ComixOneHitSounds = {}
+ComixOneHitSounds[1] = "OneHitSounds\\brutality.ogg"
+ComixOneHitSounds[2] = "OneHitSounds\\fatality.ogg"
+ComixOneHitSounds[3] = "OneHitSounds\\supurb.ogg"
+ComixOneHitSounds[4] = "OneHitSounds\\outstanding.ogg"
+ComixOneHitSounds[5] = "OneHitSounds\\excelent.ogg"
+ComixOneHitSounds[5] = "OneHitSounds\\onehit1.ogg"
+ComixOneHitSounds[5] = "OneHitSounds\\onehit2.ogg"
+
+-- Comix Healing Sounds --
+ComixHealingSounds = {}
+ComixHealingSounds[1] = "Healing\\healing1.ogg"
+ComixHealingSounds[2] = "Healing\\healing2.ogg"
+ComixHealingSounds[3] = "Healing\\healing3.ogg"
+ComixHealingSounds[4] = "Healing\\alleluia.ogg"
+
+-- Objections --
+ComixObjectionSounds = {}
+ComixObjectionSounds[1] = "ObjectionSounds\\objectionm1.ogg"
+ComixObjectionSounds[2] = "ObjectionSounds\\objectionm2.ogg"
+ComixObjectionSounds[3] = "ObjectionSounds\\objectionf1.ogg"
+ComixObjectionSounds[4] = "ObjectionSounds\\objectionf2.ogg"
